a56690442c7010483250a696925c76dcf2a6cd0b
Dan Walsh <dgwalsh1@hotmail.com>
2016-01-13 22:56:30 +0000

    Updated to plain JS, CSS included

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-var userLat, userLon, weatherAPI;
+//Geolocation & OpenWeather API project
+//Author - Dan Walsh
 
-if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-      return weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude.toString() + '&lon=' + position.coords.longitude.toString() + '&APPID=31457b046535940aeeea5440f0af36a3';
-    });
+//defining url components for later use
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
+var key = '&APPID=31457b046535940aeeea5440f0af36a3';
+
+//accessing user location through geolocation API
+function getLocation(){
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getCurrentWeatherData);
+  } else {
+    alert("Geolocation isn't supported by your browser!");
+  }
 }
 
-$(document).ready(function(){
-  $('#data').click(function(){
-    console.log(weatherAPI);
+//pulling together API call url, making request
+function getCurrentWeatherData(position){
+  var weatherAPI = baseURL + 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + key;
+  var xmlhr = new XMLHttpRequest();
+  xmlhr.onreadystatechange = function(){
+    if(xmlhr.readyState == 4 && xmlhr.status == 200){
+      var JSONobj = JSON.parse(xmlhr.responseText);
+      Parse(JSONobj);
+    }
+  }
+  xmlhr.open('GET', weatherAPI, true);
+  xmlhr.send();
+}
 
-    $.getJSON(weatherAPI, function(json){
-      var html = '';
+//inserting info into HTML 
+function Parse(obj){
+  var html = '';
 
-      html += '<p>' + json.weather[0].description + '</p>';
-      console.log(json.weather[0].description);
-      html += '<p>' + json.wind.speed + '</p>';
-      console.log(json.wind.speed);
-      html += '<p>' + json.name + '</p>';
-      console.log(json.name);
+  html += '<img id="icon" src="http://openweathermap.org/img/w/' + obj.weather[0].icon + '.png"><br>';
+  //html += '<h3>Current weather:</h3>';
+  html += '<div class="weatherResult">' + obj.weather[0].description + '</div>';
+  //html += '<h3>Current wind speed:</h3>';
+  html += '<div class="weatherResult">' + obj.wind.speed + ' m/s</div>';
+  //html += '<h3>Current location:</h3>';
+  html += '<div class="weatherResult">' + obj.name + '</div>';
 
-      $('#data').html(html);
-    });
-  });
-});
+  document.getElementById('data').innerHTML = html;
+}
