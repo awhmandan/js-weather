fed630a236adfa3c9bda06f113189ccfef71597d
Dan Walsh <dgwalsh1@hotmail.com>
2016-08-25 18:35:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,24 +2,26 @@
 //Author - Dan Walsh
 
 //defining url components for later use
-var baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
-var key = '&APPID=31457b046535940aeeea5440f0af36a3';
+var baseURL = 'https://api.wunderground.com/api/';
+var key = 'redacted';
+var cond = '/conditions/q/';
+var format = '.json';
 
 //accessing user location through geolocation API
-function getLocation(){
-  if(navigator.geolocation) {
+function getLocation() {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getCurrentWeatherData);
   } else {
     alert("Geolocation isn't supported by your browser!");
   }
 }
-
+	
 //pulling together API call url, making request
-function getCurrentWeatherData(position){
-  var weatherAPI = baseURL + 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + key;
+function getCurrentWeatherData(position) {
+  var weatherAPI = baseURL + key + cond + position.coords.latitude + ',' + position.coords.longitude + format;
   var xmlhr = new XMLHttpRequest();
-  xmlhr.onreadystatechange = function(){
-    if(xmlhr.readyState == 4 && xmlhr.status == 200){
+  xmlhr.onreadystatechange = function() {
+    if (xmlhr.readyState == 4 && xmlhr.status == 200) {
       var JSONobj = JSON.parse(xmlhr.responseText);
       Parse(JSONobj);
     }
@@ -29,16 +31,16 @@ function getCurrentWeatherData(position){
 }
 
 //inserting info into HTML 
-function Parse(obj){
+function Parse(obj) {
   var html = '';
 
-  html += '<img id="icon" src="http://openweathermap.org/img/w/' + obj.weather[0].icon + '.png"><br>';
+  html += '<img id="icon" src="' + obj.current_observation.icon_url + '"><br>';
   //html += '<h3>Current weather:</h3>';
-  html += '<div class="weatherResult">' + obj.weather[0].description + '</div>';
+  html += '<div class="weatherResult">' + obj.current_observation.weather + '</div>';
   //html += '<h3>Current wind speed:</h3>';
-  html += '<div class="weatherResult">' + obj.wind.speed + ' m/s wind speed</div>';
+  html += '<div class="weatherResult">' + obj.current_observation.wind_mph + ' m/s wind speed</div>';
   //html += '<h3>Current location:</h3>';
-  html += '<div class="weatherResult">' + obj.name + '</div>';
+  html += '<div class="weatherResult">' + obj.current_observation.display_location.full + '</div>';
 
   document.getElementById('data').innerHTML = html;
 }
